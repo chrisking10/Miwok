4cb77af30a0d9c013fd538cd673698dfcd4b2420
Chris King <chrisjking0537@gmail.com>
2017-11-30 13:49:36 -0600

    Initial

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v7.app.AppCompatActivity;
@@ -23,6 +24,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 	
@@ -32,5 +34,10 @@ public class MainActivity extends AppCompatActivity {
 		
 		// Set the content of the activity to use the activity_main.xml layout file
 		setContentView(R.layout.activity_main);
+	}
+	
+	public void openNumbersList (View view) {
+		Intent numbers =  new Intent(this, NumberActivity.class);
+		startActivity(numbers);
 	}
 }
